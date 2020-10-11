c64e98125ccd52a4423a97f95965da3595ee0710
Zugyuk <overlordmw@hotmail.com>
2020-10-10 18:39:46 -0700

    Environmental anims and prefabs

---
+++ Assets/Scripts/GrassAnimScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassAnimScript : MonoBehaviour
+{
+    private Animation anim;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.Play(0, -1, Random.value);
+    }
+
+}
