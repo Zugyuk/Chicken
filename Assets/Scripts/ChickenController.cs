c3caf9a2c767144751c0abae381e11be54e4150f
Zugyuk <overlordmw@hotmail.com>
2020-12-06 17:57:36 -0800

    Updating prefab and scene to incorporate fixed cam 2d.

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -7,20 +7,14 @@ using System;
 public class ChickenController : MonoBehaviour
 {
     // Move player in 2D space
-    [SerializeField] private float maxSpeed = 3.4f;
-    [SerializeField] private float jumpHeight = 6.5f;
-    [SerializeField] private float gravityScale = 1.5f;
-    public Camera mainCamera;
+    [SerializeField] private float currentSpeed = 5f;
     private Animator animator;
 
     bool facingRight = true;
-    private float moveDirection = 0;
-    [SerializeField] private bool isGrounded = false;
-    Vector3 cameraPos;
+    private float moveDirectionH = 0;
+    private float moveDirectionV = 0;
     Rigidbody2D r2d;
     Collider2D mainCollider;
-    // Check every collider except Player and Ignore Raycast
-    LayerMask layerMask = ~(1 << 2 | 8 << 10);
     Transform t;
 
     // Use this for initialization
@@ -30,92 +24,47 @@ public class ChickenController : MonoBehaviour
         r2d = GetComponent<Rigidbody2D>();
         mainCollider = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
-        r2d.freezeRotation = true;
-        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        r2d.gravityScale = gravityScale;
+        
         facingRight = t.localScale.x > 0;
-        gameObject.layer = 8;
-
-        if (mainCamera)
-            cameraPos = mainCamera.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        // Movement controls
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || r2d.velocity.x > 0.01f))
-        {
-            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
-            animator.SetBool("IsWalking", true);
-        }
-        else
-        {
-            if (isGrounded || r2d.velocity.magnitude < 0.01f)
-            {
-                moveDirection = 0;
-            }
-            animator.SetBool("IsWalking", false);
-        }
+    {        
+        // animate if moving
+        if (moveDirectionH != 0 || moveDirectionV != 0) {animator.SetBool("IsWalking", true);}
+        else {animator.SetBool("IsWalking", false);}
+        
+        // Movement controls that trigger move direction variables.
+        if (Input.GetKey(KeyCode.D)) {moveDirectionH = 1;}
+        else if(Input.GetKey(KeyCode.A)) {moveDirectionH = -1;}
+        else {moveDirectionH = 0;}
 
-        // Change facing direction
-        if (moveDirection != 0)
+        if (Input.GetKey(KeyCode.W)){moveDirectionV = 1;}
+        else if (Input.GetKey(KeyCode.S)) {moveDirectionV = -1;}
+        else {moveDirectionV = 0;}
+
+        // facing
+        if (moveDirectionH != 0)
         {
-            if (moveDirection > 0 && !facingRight)
+            if (moveDirectionH > 0 && !facingRight)
             {
                 facingRight = true;
-                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
+                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
             }
-            if (moveDirection < 0 && facingRight)
+            else if (moveDirectionH < 0 && facingRight)
             {
                 facingRight = false;
                 t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
             }
         }
-
-        // Duck
-        if (Input.GetKey(KeyCode.S) && isGrounded)
-        {
-            animator.SetBool("IsDucking", true);
-        }
-        
-        if (!Input.GetKey(KeyCode.S) && isGrounded)
-        {
-            animator.SetBool("IsDucking", false);
-        }
-
-        // Jumping
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
-        {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded == false)
-        {
-            r2d.gravityScale = gravityScale * 0.5f;
-        }
-        
-        if (isGrounded && r2d.gravityScale < 1.0f)
-        {
-            r2d.gravityScale = gravityScale;
-        }
-
-        // Camera follow
-        if (mainCamera)
-            mainCamera.transform.position = new Vector3(t.position.x, cameraPos.y, cameraPos.z);
     }
 
     void FixedUpdate()
     {
         Bounds colliderBounds = mainCollider.bounds;
-        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, -0.1f, 0);
-        // Check if player is grounded
-        isGrounded = Physics2D.OverlapCircle(groundCheckPos, 0.1f, layerMask);
 
         // Apply movement velocity
-        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
-
-        // Simple debug
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, 0.23f, 0), isGrounded ? Color.green : Color.red);
+        r2d.velocity = new Vector2((moveDirectionH * currentSpeed), (moveDirectionV * currentSpeed));
+        print(r2d.velocity);
     }
 }
